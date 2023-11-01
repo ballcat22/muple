dfd4de8eb619b8e4b5dde25efd110b67c7003614
ballcat22 <145007797+ballcat22@users.noreply.github.com>
2023-11-01 17:47:26 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// main new 종합 / 국내 / 해외
 $(function () {
 	$("#button1").click(function () {
 
@@ -15,5 +16,15 @@ $(function () {
 
 $(".new_link li a").click(function () {
 	$(this).parent().addClass("on").siblings().removeClass("on")
+});
+
+
+
+// festival faq 토글 - 슬라이드, 배경 변경
+$(function(){
+	$(".faqs li .q").click(function(){
+		$(this).next().slideToggle();
+		$(this).parent().toggleClass("minus");
+	});
 });
 
